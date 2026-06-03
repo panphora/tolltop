44c96fd340e3b0e5311c8b2e74928cc0d4d95043
David Miranda <david@storylog.com>
2026-06-03 13:13:23 -0400

    tolltop 3.0.2

--- tolltop.js
+++ tolltop.js
@@ -1,5 +1,5 @@
 /*!
- * tolltop v3.0.1
+ * tolltop v3.0.2
  * Tiny edge-aware tooltips with smart positioning. One attribute, one config call.
  * MIT License · https://github.com/panphora/tolltop
  */
